c41e53e1ac48b1381d22f0e555922d6238ccbb84
MageswaranA <65114150+MageswaranA@users.noreply.github.com>
2024-01-15 13:51:14 +0530

    Create helloJava

---
+++ helloJava.java
@@ -0,0 +1,7 @@
+//Sample Java Program
+class hellojava { 
+  public static void main(String[] args) { 
+    System.out.println("Hello, World!"); 
+    System.out.println("Welcome to GitHub Actions"); 
+  } 
+}
